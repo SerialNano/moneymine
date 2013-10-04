6715fc78d97ced5aaa08965935802b09dba6143a
Joshua T <jxthibeault@icloud.com>
2013-10-04 13:56:10 -0400

    Oct 4 2013, Commit 2

--- src/com/icloud/jxthibeault/moneymine/FileSysManager.java
+++ src/com/icloud/jxthibeault/moneymine/FileSysManager.java
@@ -1,5 +1,46 @@
 package com.icloud.jxthibeault.moneymine;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+
 public class FileSysManager {
+	public void createFile(String filepath){
+		PrintWriter creator = null;
+		try {
+			creator = new PrintWriter(filepath, "UTF-8");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		creator.close();
+	}
 	
+	public void appendFile(String filepath){
+		File target = new File(filepath);
+		target.delete();
+	}
+	
+	public void writeFile(String filepath, String dataText){
+		FileWriter appender = null;
+		try {
+			appender = new FileWriter(filepath, true);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		try {
+			appender.write(dataText);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		try {
+			appender.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
