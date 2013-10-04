405d258c8e37e838d83bea329187e0d441fd4d05
Joshua T <jxthibeault@icloud.com>
2013-10-04 15:51:21 -0400

    October 4 2013, Commit 3
    - Added onEnable() to main class file.

--- src/com/icloud/jxthibeault/moneymine/Moneymine.java
+++ src/com/icloud/jxthibeault/moneymine/Moneymine.java
@@ -1,7 +1,17 @@
 package com.icloud.jxthibeault.moneymine;
 
+import java.io.File;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Moneymine extends JavaPlugin{
-
+	
+	Stringw[] users = new String[]{};
+	File userDirectory = new File("plugins/moneymine/users");
+	
+	public void onEnable(){
+		getLogger().info("Loading users from file...");
+		File[] users = userDirectory.listFiles();
+	}
+	
 }
