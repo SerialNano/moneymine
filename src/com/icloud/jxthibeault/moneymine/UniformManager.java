2d587eb062de3e3814de931b970c325d20405a9d
Joshua T <jxthibeault@icloud.com>
2013-11-27 09:25:26 -0500

    Nov 27 2013, Commit 1
    -Finished removal of the buggy code
    +Began work on the UniformManagement class

---
+++ src/com/icloud/jxthibeault/moneymine/UniformManager.java
@@ -0,0 +1,87 @@
+package com.icloud.jxthibeault.moneymine;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.PlayerInventory;
+
+public class UniformManager{
+	
+	public static void createTable(){
+		Connection c = null;
+		Statement stmt = null;
+		try{
+			Class.forName("org.sqlite.JDBC");
+			c = DriverManager.getConnection("jdbc:sqlite:plugins/moneymine/moneymine.db");
+			stmt = c.createStatement();
+			String sql = "CREATE TABLE uniforms " + "(job TEXT PRIMARY KEY," + " hat TEXT," + " shirt TEXT," + " pants TEXT," + " shoes TEXT);";
+			stmt.executeUpdate(sql);
+			stmt.close();
+			c.close();
+		} catch (Exception e) {}
+	}
+	
+	public static void updateUniform(String job, String hat, String shirt, String pants, String shoes){
+		Connection c = null;
+		Statement stmt = null;
+		try{
+			Class.forName("org.sqlite.JDBC");
+			c = DriverManager.getConnection("jdbc:sqlite:plugins/moneymine/moneymine.db");
+			c.setAutoCommit(false);
+			
+			stmt = c.createStatement();
+			String sql = "UPDATE uniforms set hat = " + hat + " where job=" + job +";";
+			stmt.executeUpdate(sql);
+			c.commit();
+			
+			sql = "UPDATE uniforms set shirt = " + shirt + " where job=" + job +";";
+			stmt.executeUpdate(sql);
+			c.commit();
+			
+			sql = "UPDATE uniforms set pants = " + pants + " where job=" + job +";";
+			stmt.executeUpdate(sql);
+			c.commit();
+			
+			sql = "UPDATE uniforms set shoes = " + shoes + " where job=" + job +";";
+			stmt.executeUpdate(sql);
+			c.commit();
+		} catch (Exception E){}
+	}
+	
+	@SuppressWarnings("null")
+	public static void giveUniform(Player p, String job){
+		PlayerInventory pi = p.getInventory();
+		
+		String hat = null;
+		String shirt = null;
+		String pants = null;
+		String shoes = null;
+		
+		String jobRead = null;
+		
+		Connection c = null;
+		Statement stmt = null;
+		
+		try{
+			ResultSet rs = stmt.executeQuery("SELECT * FROM uniforms;");
+			while ( rs.next() ){
+				jobRead = rs.getString("job");
+				hat = rs.getString("hat");
+				shirt = rs.getString("shirt");
+				pants = rs.getString("pants");
+				shoes = rs.getString("shoes");
+				
+				if(jobRead == job){
+					//Give player armour--place directly into armour slots
+				}
+			}
+			rs.close();
+			stmt.close();
+			c.close();
+		} catch (Exception E){}
+	}
+	
+}
